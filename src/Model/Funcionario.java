1b2fd69b622587e357db4fcfd6cce566f29c4409
Sarah Freitas <sarahjfreitas@gmail.com>
2019-11-27 19:57:13 -0300

    employer class creation

---
+++ src/Model/Funcionario.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class Funcionario {
+	private int codigo;
+	private String nome;
+	private String telefone;
+	private String cargo;
+	private double salario;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+}
