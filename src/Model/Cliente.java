f2f892c1a94dfe7269f05bab99dac35607913586
Sarah Freitas <sarahjfreitas@gmail.com>
2019-11-27 19:48:37 -0300

    cliente class creation

---
+++ src/Model/Cliente.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class Cliente {
+	private int codigo;
+	private String nome;
+	private String endereco;
+	private String telefone;
+	
+	public Cliente() {};
+	
+	public Cliente(int codigo, String nome, String endereco, String telefone) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
