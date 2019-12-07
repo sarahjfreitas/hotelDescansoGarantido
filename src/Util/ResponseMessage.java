fad8bd663803b9a3cf1b6d85ae89846450b605d9
Sarah Julia <sarahjfreitas@gmail.com>
2019-12-07 17:58:26 -0300

    documentacao das classes utilities

--- src/Util/ResponseMessage.java
+++ src/Util/ResponseMessage.java
@@ -1,12 +1,28 @@
 package Util;
 
+/**
+ * Classe para objeto de resposta com status e mensagem
+ *
+ * @author Sarah Júlia
+ */
 public class ResponseMessage {
-	public boolean status;
-	public String message;
-	
-	public ResponseMessage() {}
-	public ResponseMessage(boolean status, String message) {
-		this.status = status;
-		this.message = message;
-	}
+
+    public boolean status;
+    public String mensagem;
+
+    /**
+     * contrutor padrao de objeto vazio
+     */
+    public ResponseMessage() {
+    }
+
+    /**
+     * contrutor para criação de objeto com os dados
+     * @param status boolean
+     * @param mensagem String
+     */
+    public ResponseMessage(boolean status, String mensagem) {
+        this.status = status;
+        this.mensagem = mensagem;
+    }
 }
--- src/Util/Utilities.java
+++ src/Util/Utilities.java
@@ -1,23 +1,39 @@
 package Util;
 
+/**
+ * Classe de utilidades diversas
+ *
+ * @author Sarah Júlia
+ */
 public class Utilities {
-	public static boolean isInteger(String s) {
-	    try { 
-	        Integer.parseInt(s); 
-	    }
-	    catch(Exception e) { 
-	        return false; 
-	    }
-	    return true;
-	}
-	
-	public static boolean isDouble(String s) {
-		try {
-			Double.parseDouble(s);
-		}
-	    catch(Exception e) { 
-	        return false; 
-	    }
-		return true;
-	}
+
+    /**
+     * metodo que verifica se a string passada é um numero inteiro
+     *
+     * @param s String a ser testada
+     * @return
+     */
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * metodo que verifica se a string passada é um numero do tipo double
+     *
+     * @param s String a ser testada
+     * @return
+     */
+    public static boolean isDouble(String s) {
+        try {
+            Double.parseDouble(s);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
